c21984701f8c1f5c7d76bd06ddab5dbaf20607a8
taolin204 <tao.lin@outlook.com>
2018-08-09 19:48:25 +0800

    Component Test

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,16 +1,77 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './menu.component';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { DISHES } from '../shared/dishes';
+import { baseURL } from '../shared/baseurl';
+import { Observable, of } from 'rxjs';
+
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [ MenuComponent ]
+  //   })
+  //   .compileComponents();
+  // }));
+
   beforeEach(async(() => {
+    let dishServiceStub = {
+      getDishes: function (): Observable<Dish[]> {
+        return of(DISHES);
+      }
+    };
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      imports: [
+        BrowserAnimationsModule,
+        MatGridListModule,
+        MatCardModule,
+        MatButtonModule,
+        MatDialogModule,
+        MatToolbarModule,
+        MatListModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCheckboxModule,
+        MatSelectModule,
+        MatSlideToggleModule,
+        MatSliderModule,
+        FlexLayoutModule,
+        RouterTestingModule.withRoutes([{
+          path: 'menu', component: MenuComponent
+        }])
+      ],
+      declarations: [MenuComponent],
+      providers: [
+        { provide: DishService, useValue: dishServiceStub },
+        { provide: 'BaseURL', useValue: baseURL },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+    let dishservice = TestBed.get(DishService);
   }));
 
   beforeEach(() => {
@@ -22,4 +83,20 @@ describe('MenuComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('dishes items should be 4', () => {
+    expect(component.dishes.length).toBe(4);
+    expect(component.dishes[1].name).toBe('Zucchipakoda');
+    expect(component.dishes[3].featured).toBeFalsy();
+  });
+
+  it('should use dishes in the template', () => {
+    fixture.detectChanges();
+    let de: DebugElement;
+    let el: HTMLElement;
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+    expect(el.textContent).toContain(DISHES[0].name.toUpperCase());
+  });
+
 });
